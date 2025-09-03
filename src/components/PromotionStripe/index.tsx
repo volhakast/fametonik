23477970f2f23dcf78508c0bfcd033215ee09d46
OlyaKastsiukova <volhakastsiukova@gmail.com>
2025-09-03 09:42:31 +0300

    Fix leading

--- src/components/PromotionStripe/index.tsx
+++ src/components/PromotionStripe/index.tsx
@@ -1,11 +1,11 @@
 function PromotionStripe() {
   return (
-      <div className="gradient-bg flex items-center text-white text-center bg-gradient-primary py-5 px-5 lg:py-1.5">
+      <div className="gradient-bg flex items-center text-white text-center bg-gradient-primary py-5 px-5 lg:py-2">
           <div className="max-w-main mx-auto">
               <h3 className="leading-none">
-                  <span className="text-base font-extrabold text-light-blue lg:text-22">🚀 FRESH BEGINNINGS SALE:</span>
+                  <span className="text-base font-extrabold leading-none text-light-blue lg:text-22">🚀 FRESH BEGINNINGS SALE:</span>
                   {' '}
-                  <span className="text-base font-semibold text-white text-sm lg:text-22">Extra 25% OFF, Limited Spots - start your journey today!</span>
+                  <span className="text-base font-semibold leading-none text-white text-sm lg:text-22">Extra 25% OFF, Limited Spots - start your journey today!</span>
               </h3>
           </div>
       </div>
